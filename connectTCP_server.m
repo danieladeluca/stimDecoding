6bf0c4a1abcaa0df33c667961e89841ec81b338e
Leonardo <leonardo.lupori@gmail.com>
2022-01-06 20:36:53 +0100

    Update matlab acquisition files

--- connectTCP_server.m
+++ connectTCP_server.m
@@ -1,13 +1,9 @@
-function tcp = connectTCP_server(IP, port, bufferSize)
+function tcp = connectTCP_server(IP, port)
 
 
 fprintf(['Starting TCP/IP server on [Address: ' IP...
     ' - Port: %u]...'], port)
-try
-    tcp = tcpip(IP, port, 'NetworkRole', 'server');
-    fprintf(' started.\n')
-catch me
-    fprintf(' failed.  Aborting the experiment \n')
-end
-set(tcp, 'InputBufferSize', bufferSize);
-fprintf('Server Ready! waiting for connection to Psychopy client...')
+
+tcp = tcpserver(IP, port);
+
+fprintf('Server Ready! waiting for connection from clients...\n')
--- stimDecodingServer.m
+++ stimDecodingServer.m
@@ -14,22 +14,48 @@ settings.savingFolder = 'C:\Users\Leonardo\Documents\';
 % TCP/IP SETTINGS
 % -------------------------------------------------------------------------
 settings.tcp.address = 'localhost';
-settings.tcp.port = 80;
-settings.tcp.bufferSize = 4096;
+settings.tcp.port = 40000;
+
+
 
 %% Connect to the camera and initialize it
 [vid, src] = loadCamera_webcam();
 
+totalDuration = settings.preStim + settings.durStim + settings.postStim;  
+vid.FramesPerTrigger = round(totalDuration * str2double(src.FrameRate));
+
 %% Setup TCP/IP connection with the psychopy instance on localhost
-tcp = connectTCP_server(settings.tcp.address, settings.tcp.port, settings.tcp.bufferSize);
+tcp = connectTCP_server(settings.tcp.address, settings.tcp.port);
 
 
-
-
-
-
-
+%%
 stimList = pseudorandomSequence(settings.stimuli, settings.repetitions);
+
+
+for i = 1:2
+    fprintf('Trial [%u/%u]...', i, length(stimList))
+
+    start(vid)
+    % Send current trial to python that whil trigget the camera
+    write(tcp, stimList{i})
+    trigger(vid)
+    
+    % Wait for the acquisition to finish
+    wait(vid,25)
+    fprintf(' done. Processing...')
+    % Preprocess data, save it and display preview
+    [data,time] = getdata(vid);
+    stop(vid)
+
+    fprintf('done.\n')
+end
+
+
+
+%%
+
+
+
 
 
 
