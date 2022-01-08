d04a3b6bb48c9ff3c5b57cef6e35c752ef620f1d
master <master@DESKTOP-V33JHQA>
2022-01-08 16:04:08 +0100

    now the recording is working

--- updatePreviewFigure.m
+++ updatePreviewFigure.m
@@ -1,10 +1,13 @@
-function updatePreviewFigure(imHandle, data, newData, iterNumber)
+function newSumImg = updatePreviewFigure(imHandle, data, newData, iterNumber)
 
 newData = double(newData);
 df = dRoR(newData, newData(:,:,1:10));
 
 df = mean(df(:,:,10:20),3);
-totalAverage = (data + df) / iterNumber;
+newSumImg = (data + df);
+totalAverage = newSumImg / iterNumber;
+totalAverage = imgaussfilt(totalAverage,2);
 imHandle.CData = totalAverage;
+caxis(imHandle.Parent, [min(totalAverage(:)),max(totalAverage(:))]); 
 
 
