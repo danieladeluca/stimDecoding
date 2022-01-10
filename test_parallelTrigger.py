a5f5b80cef22164d35b7171754e5880bb825115b
Leonardo Lupori <leonardo.lupori@gmail.com>
2022-01-10 17:52:44 +0100

    clean some old test scripts

--- test_parallelTrigger.py
+++ test_parallelTrigger.py
@@ -1,6 +1,11 @@
-from psychopy import core, parallel
-import time
+"""
+TEST SCRIPT
+IT can be usefult to send specific signals to the parallel port to test the 
+proper functioning of triggered equipment.
+"""
 
+from psychopy import parallel
+import time
 
 pPort = parallel.ParallelPort(address = '0xD010')
 # pPort.setData(int("00000000",2))
@@ -9,5 +14,4 @@ for _ in range(1):
     pPort.setData(int("00001000",2))
     time.sleep(0.1)
     pPort.setData(int("00000001",2))
-
     time.sleep(1)
